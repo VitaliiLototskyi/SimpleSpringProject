03d12f4037081f6c34972ca519dabfef4d4c702c
Vitalii <loto9308@gmail.com>
2019-01-30 18:07:30 +0200

    deleted unused imports

--- src/main/java/elastic/Controller.java
+++ src/main/java/elastic/Controller.java
@@ -1,38 +1,23 @@
 package elastic;
 
-import jdk.nashorn.api.scripting.JSObject;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.elasticsearch.action.get.GetRequest;
-import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.reindex.BulkByScrollResponse;
 import org.elasticsearch.index.reindex.DeleteByQueryAction;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Date;
-import java.util.List;
 import java.util.Properties;
-import java.util.UUID;
-
-import static org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder;
 
 public class Controller {
     public static void main(String[] args) throws IOException {
